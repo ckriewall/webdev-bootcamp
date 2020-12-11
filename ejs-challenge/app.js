9c81ba2f7d08277bb4003fb2da2a0161db3aa490
Charlie Kriewall <charlie@kriewall.com>
2020-12-11 13:26:02 -0800

    ejs-challenge 9-11 push new posts to global array

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -11,6 +11,8 @@ const aboutContent =
 const contactContent =
   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.'
 
+const posts = []
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -19,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  console.log(posts)
   res.render('home', { homeContent: homeContent })
 })
 
@@ -35,9 +38,9 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-  const newPost = req.body.postTitle
-  console.log(newPost)
-  res.send(newPost)
+  const post = { title: req.body.postTitle, body: req.body.postBody }
+  posts.push(post)
+  res.redirect('/')
 })
 
 app.listen(port, () => {
